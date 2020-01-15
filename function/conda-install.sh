e360adc6c6f01392144ffc291a523d3d0160f74d
Hai Ninh Hoang <haininhhoang94@gmail.com>
2020-01-15 16:23:24 +0700

    continue

---
+++ function/conda-install.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+conda install -y numpy pandas matplotlib seaborn scikit-learn
+conda install -c conda-forge jupyterlab nodejs
---
+++ function/install-necessary.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+sudo apt install -y git zsh vim 
+sudo apt install -y nodejs npm
+sudo apt install -y jupyter jupyter-core
