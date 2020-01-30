8de75c19b1c031ed5471f69282a810f76f1188d8
Hai Ninh Hoang <haininhhoang94@gmail.com>
2020-01-30 01:13:02 -0800

    update ycm config

--- function/backup-config.sh
+++ function/backup-config.sh
@@ -3,3 +3,4 @@ cp ~/.vimrc ../backup/vimrc.bak
 cp ~/.tmux.conf ../backup/tmux.conf.bak
 cp ~/.zshrc ../backup/zshrc.conf.bak
 cp ~/.aliases ../backup/aliases.bak
+cp ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py ../backup/ycm_extra_conf.py
--- function/restore-config.sh
+++ function/restore-config.sh
@@ -3,3 +3,4 @@ cp ../backup/vimrc.bak ~/.vimrc
 cp ../backup/tmux.conf.bak ~/.tmux.conf
 cp ../backup/zshrc.conf.bak ~/.zshrc
 cp ../backup/aliases.bak ~/.aliases
+cp ../backup/ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
