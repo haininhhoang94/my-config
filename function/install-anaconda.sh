28ec451451003e1bb6ad3a2cd450394d1fe3fb22
Hai Ninh Hoang <haininhhoang94@gmail.com>
2020-02-02 14:49:14 +0700

    update

---
+++ function/install-anaconda.sh
@@ -0,0 +1,2 @@
+mkdir ~/Downloads
+wget 
--- function/install-oh-my-zsh.sh
+++ function/install-oh-my-zsh.sh
@@ -1,1 +1,1 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
