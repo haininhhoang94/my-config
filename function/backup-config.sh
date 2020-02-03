fe5dc16f707d773e98889b8cd7ff7df771f8be13
Hai Ninh Hoang <haininhhoang94@gmail.com>
2020-02-03 23:26:30 +0700

    ssh copy

--- function/backup-config.sh
+++ function/backup-config.sh
@@ -4,3 +4,4 @@ cp ~/.tmux.conf ../backup/tmux.conf.bak
 cp ~/.zshrc ../backup/zshrc.conf.bak
 cp ~/.aliases ../backup/aliases.bak
 cp ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py ../backup/ycm_extra_conf.py
+#cp ~/.ssh/config ../backup/config.bak
