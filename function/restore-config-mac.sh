201caa854b816fe6be07e348682e8162092fa969
Hai Ninh Hoang <haininhhoang94@MacBook15.local>
2020-02-05 18:57:56 +0700

    update

---
+++ function/restore-config-mac.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+# copy to backup-mac first
+cp ../backup/vimrc.bak ../backup-mac/vimrc.bak
+cp ../backup/tmux.conf.bak ../backup-mac/tmux.conf.bak
+cp ../backup/zshrc.conf.bak ../backup-mac/zshrc.bak
+cp ../backup/aliases.bak ../backup-mac/aliases.bak
+cp ../backup/ycm_extra_conf.py ../backup-mac/ycm_extra_conf.py
+
+# edit the backup-mac file
+sed -i '' 's/\/home/\/Users/g' ../backup-mac/*
+
+cp ../backup-mac/vimrc.bak ~/.vimrc
+cp ../backup-mac/tmux.conf.bak ~/.tmux.conf
+cp ../backup-mac/zshrc.bak ~/.zshrc
+cp ../backup-mac/aliases.bak ~/.aliases
+cp ../backup-mac/ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
