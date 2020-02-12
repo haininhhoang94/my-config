86ee67c03eaa41b8640bc7e4ff4adda06a1e76b0
Hai Ninh Hoang <haininhhoang94@gmail.com>
2020-02-12 17:05:43 +0700

    update

--- function/backup-config.sh
+++ function/backup-config.sh
@@ -3,5 +3,5 @@ cp ~/.vimrc ../backup/vimrc.bak
 cp ~/.tmux.conf ../backup/tmux.conf.bak
 cp ~/.zshrc ../backup/zshrc.conf.bak
 cp ~/.aliases ../backup/aliases.bak
-cp ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py ../backup/ycm_extra_conf.py
+cp ~/.config/nvim/plugged/YouCompleteMe/ycm_extra_conf.py ../backup/ycm_extra_conf.py
 cp ~/.config/nvim/init.vim ../backup/init.vim.bak
--- function/install-nvim.sh
+++ function/install-nvim.sh
@@ -1,4 +1,11 @@
 #/bin/bash
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt update
-sudo apt install -y neovim
+#sudo add-apt-repository ppa:neovim-ppa/stable
+#sudo apt update
+#sudo apt install -y neovim
+sudo apt install cmake automake libtool libtool-bin gettext
+cd ~/Downloads
+git clone https://github.com/neovim/neovim.git
+cd neovim
+make -j CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+pip3 install --user neovim
