f90a1e71a938ab855d69564d4efc568b2e147c4b
haininhhoang94@gmail.com <haininhhoang94@gmail.com>
2020-02-15 12:05:57 +0700

    test

--- function/restore-config.sh
+++ function/restore-config.sh
@@ -4,3 +4,4 @@ cp ../backup/tmux.conf.bak ~/.tmux.conf
 cp ../backup/zshrc.conf.bak ~/.zshrc
 cp ../backup/aliases.bak ~/.aliases
 cp ../backup/ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
+cp ../backup/init.vim.bak ~/.config/nvim/init.vim
