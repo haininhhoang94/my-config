3347dec9fdb91b920c75b4dde65664d400cce8db
Hai Ninh Hoang <haininhhoang94@MacBook15.local>
2020-02-17 09:41:48 +0700

    haha

--- function/backup-config.sh
+++ function/backup-config.sh
@@ -3,5 +3,4 @@ cp ~/.vimrc ../backup/vimrc.bak
 cp ~/.tmux.conf ../backup/tmux.conf.bak
 cp ~/.zshrc ../backup/zshrc.conf.bak
 cp ~/.aliases ../backup/aliases.bak
-cp ~/.config/nvim/plugged/YouCompleteMe/ycm_extra_conf.py ../backup/ycm_extra_conf.py
 cp ~/.config/nvim/init.vim ../backup/init.vim.bak
