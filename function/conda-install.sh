1696a57d7016983b73fd379f19464a26b0fcdf8c
Hai Ninh Hoang <haininhhoang94@gmail.com>
2020-02-23 22:53:16 +0700

    update

--- function/conda-install.sh
+++ function/conda-install.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-#conda create --name pyds
+conda create --name pyds
 conda install -y numpy pandas matplotlib seaborn scikit-learn jupyter scipy
 conda install -c conda-forge jupyterlab nodejs
