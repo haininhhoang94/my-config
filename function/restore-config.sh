a6f9c55913f7e8d2735509d65a9f28410b3ace2a
Hai Ninh Hoang <haininhhoang94@gmail.com>
2020-05-15 22:12:13 +0700

    backup before trying coc-vim

--- function/restore-config.sh
+++ function/restore-config.sh
@@ -5,6 +5,9 @@ cp ../backup/zshrc.conf.bak ~/.zshrc
 cp ../backup/aliases.bak ~/.aliases
 #cp ../backup/ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
 cp ../backup/init.vim.bak ~/.config/nvim/init.vim
+cp ../backup/plugins.vim.bak ~/.config/nvim/plugins.vim
+cp ../backup/general.vim.bak ~/.config/nvim/general.vim
+cp ../backup/keybinding.vim.bak ~/.config/nvim/keybinding.vim
+cp ../backup/theme.vim.bak ~/.config/nvim/theme.vim
 #cp ../backup/alacritty.yml.bak ~/.config/alacritty/alacritty.yml
 cp ../backup/code-shell ~/code-shell
-cp ../backup/plugins.vim.bak ~/.config/nvim/plugins.vim
